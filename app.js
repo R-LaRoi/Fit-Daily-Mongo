c8e1056f4596e8fa7b4150e41260cd08c1fe27b1
R. LaRoi <are@raewhoo.com>
2024-07-12 11:51:43 -0400

    Added readFileSync

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const dailyDataFile = "./data/dailyData";
+const dailyRoutines = require(dailyDataFile);
+
 const dailyData = require("./data/dailyData");
 const fitUsers = require("./data/user");
 
@@ -11,24 +17,70 @@ app.get("/", (req, res) => {
   });
 });
 
+// shows users previous routine history
 app.get("/dailyroutines", function (req, res) {
   res.render("dailyroutines", dailyData);
 });
 
+//form - to add a routine
 app.get("/addroutine", function (req, res) {
   res.render("addroutine");
 });
 
-//  routines routes
+//  maka method to delete a routine....
+
+//  routines JSON data route
 app.get("/routines", (req, res) => {
   const routine = dailyData;
   res.json(routine);
 });
 
-//  user route
+//  users JSON data route
 app.get("/user", (req, res) => {
   res.json(fitUsers);
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post("/submitRoutine", (req, res) => {
+  let data = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/dailyData.json`, "utf-8")
+  );
+
+  const formData = req.body;
+  // console.log(routineData);
+  // const routineData = JSON.stringify(formData);
+  //  need to add this routine data to daily data ....
+  let userRoutine = {
+    date: formData.date,
+    type: formData.type,
+    duration: formData.duration,
+    routine: formData.message,
+  };
+
+  data = [...data, userRoutine];
+
+  fs.writeFile(
+    `${__dirname}/data/dailyData.json`,
+    JSON.stringify(data),
+    (err) => {
+      console.log("Something went wrong Mr. ES6-2015, get it right!");
+    }
+  );
+
+  res.send(`<main style="text-align: center><h1 style="text-align: center; 
+    margin-top: 50vh; transform: translateY(-50%);">
+    FIT FOR LIFE!</h1>
+    <div>${formData.date}<div>
+        <div>${formData.type}<div>
+            <div>${formData.duration}<div>
+                <div>${formData.routine}<div>
+    <a href="dailyroutines">routines</a></main>
+    `);
+});
+
+// add a delete path on daily routines
 
 //  middleware
 app.use((err, req, res, next) => {
--- data/dailyData.js
+++ data/dailyData.js
@@ -1,18 +1,18 @@
 const routines = [
   {
-    id: 0o1,
+    id: "0o1",
     day: "date",
     duration: "50 min",
     routine: ["jump rope", "kb swings", "mountain climbers"],
   },
   {
-    id: 0o2,
+    id: "0o2",
     day: "date",
     duration: "50 min",
     routine: ["jump rope", "burpees", "side climbers"],
   },
   {
-    id: 0o3,
+    id: "0o3",
     day: "date",
     duration: "50 min",
     routine: ["jump rope", "devil press", "high knees"],
