2d467684fa8e8e4e6c23b295e67c47ff1c7b2a40
R. LaRoi <are@raewhoo.com>
2024-07-14 17:06:47 -0400

    readme

--- data/dailyData.js
+++ data/dailyData.js
@@ -1,19 +1,19 @@
 const routines = [
   {
     id: "0o1",
-    day: "date",
+    day: "2024-07-12",
     duration: "50 min",
     routine: ["jump rope", "kb swings", "mountain climbers"],
   },
   {
     id: "0o2",
-    day: "date",
+    day: "2024-07-06",
     duration: "50 min",
     routine: ["jump rope", "burpees", "side climbers"],
   },
   {
     id: "0o3",
-    day: "date",
+    day: "2024-07-03",
     duration: "50 min",
     routine: ["jump rope", "devil press", "high knees"],
   },
